d223d6dccb0120682b616e21429b839857181331
정재원 <101786715+JaeWonna@users.noreply.github.com>
2022-05-19 16:49:12 +0900

    Create 프린터.py

---
+++ Programmers/스택 & 큐/프린터.py
@@ -0,0 +1,20 @@
+def solution(priorities, location):
+  answer = 0
+  from collections import deque
+
+  d = deque([(v,i) for i,v in enumerate(priorities)])
+  print(d)
+
+  while len(d):
+      item = d.popleft()
+      if d and max(d)[0] > item[0]:
+          d.append(item)
+      else:
+          answer += 1
+          if item[1] == location:
+              break
+  return answer
+
+p = [2,1,3,2]
+l = 2
+print(solution(p, l))
