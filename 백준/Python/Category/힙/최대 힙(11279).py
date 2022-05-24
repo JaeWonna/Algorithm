7ea29cff336c7f555676311a341b5da263560c44
정재원 <101786715+JaeWonna@users.noreply.github.com>
2022-05-24 13:35:53 +0900

    Create 최대 힙(11279).py

---
+++ 백준/Python/Category/힙/최대 힙(11279).py
@@ -0,0 +1,16 @@
+import heapq, sys
+
+input = sys.stdin.readline
+n = int(input())
+heap = []
+
+for _ in range(n):
+    num = int(input())
+
+    if num == 0:
+        if len(heap) == 0:
+            print(0)
+        else:
+            print(heapq.heappop(heap)*(-1))
+    else:
+        heapq.heappush(heap, num*(-1))
