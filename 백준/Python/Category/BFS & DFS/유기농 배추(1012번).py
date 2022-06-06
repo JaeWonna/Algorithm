a3ce6eb6d675738ffa50a24d1054c9c15315f1ba
정재원 <101786715+JaeWonna@users.noreply.github.com>
2022-06-06 16:01:58 +0900

    Update 유기농 배추(1012번).py

--- 백준/Python/Category/BFS & DFS/유기농 배추(1012번).py
+++ 백준/Python/Category/BFS & DFS/유기농 배추(1012번).py
@@ -1,21 +1,28 @@
+'''
+문제의 포인트는 세로 가로를 헷갈리지 않는것
+x,y대신 a,b사용한것
+'''
+
+
 from collections import deque
 
-dx = [0,0,1,-1]
-dy = [1,-1,0,0]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
 t = int(input())
 
-def bfs(graph, a, b):
+def bfs(graph, a,b):
     queue = deque()
-    queue.append((a,b))
     graph[a][b] = 0
+    queue.append((a,b))
 
     while queue:
-        x, y = queue.popleft()
+        x,y = queue.popleft()
         for i in range(4):
-            nx = x+dx[i]
-            ny = y+dy[i]
-            if nx < 0 or nx >=n or ny < 0 or ny >= m:
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
                 continue
             if graph[nx][ny] == 1:
                 graph[nx][ny] = 0
@@ -24,16 +31,16 @@ def bfs(graph, a, b):
 
 for i in range(t):
     cnt = 0
-    n, m, k = map(int,input().split())
+    m,n,k = map(int, input().split())
     graph = [[0]*m for _ in range(n)]
 
     for j in range(k):
-        x, y = map(int, input().split())
-        graph[x][y] = 1
-
+        a,b = map(int, input().split())
+        graph[b][a] = 1
+    
     for a in range(n):
         for b in range(m):
             if graph[a][b] == 1:
-                bfs(graph, a, b)
+                bfs(graph, a,b)
                 cnt += 1
     print(cnt)
