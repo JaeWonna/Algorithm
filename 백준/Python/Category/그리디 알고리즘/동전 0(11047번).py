fc67c0bfd81d1fb9a296ffca517a319bd4ee13a9
정재원 <101786715+JaeWonna@users.noreply.github.com>
2022-06-14 17:11:06 +0900

    Create 동전 0(11047번).py

---
+++ 백준/Python/Category/그리디 알고리즘/동전 0(11047번).py
@@ -0,0 +1,14 @@
+n,k = map(int, input().split())
+coin_types = []
+count = 0
+
+for i in range(n):
+    coin_types.append(int(input()))
+
+coin_types.reverse()
+
+for coin in coin_types:
+    count += k // coin
+    k %= coin
+
+print(count)
