90691d5e75fea885fc50d786eac713a57d87bd24
정재원 <101786715+JaeWonna@users.noreply.github.com>
2022-06-19 12:50:45 +0900

    Create 회의실 배정(1931번).py

---
+++ 백준/Python/Category/그리디 알고리즘/회의실 배정(1931번).py
@@ -0,0 +1,21 @@
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+time = []
+
+for i in range(n):
+    a,b = map(int, input().split())
+    time.append([a,b])
+
+time.sort(key = lambda x : (x[1], x[0]))
+
+cnt = 1
+end_time = time[0][1]
+
+for i in range(1,n):
+    if time[i][0] >= end_time:
+        cnt += 1
+        end_time = time[i][1]
+
+print(cnt)
