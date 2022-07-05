02e463808aaaf55caaed15cf43d1fc0b6b83c864
정재원 <101786715+JaeWonna@users.noreply.github.com>
2022-07-05 15:30:15 +0900

    Update 1,2,3 더하기 4(15989번).py

--- 백준/Python/Category/다이나믹 프로그래밍 DP/1,2,3 더하기 4(15989번).py
+++ 백준/Python/Category/다이나믹 프로그래밍 DP/1,2,3 더하기 4(15989번).py
@@ -3,11 +3,8 @@ input = sys.stdin.readline
 
 dp = [1]*10001
 
-cnt = 0
 for i in range(2,10001):
-    if i % 2 == 0:
-        cnt += 1
-    dp[i] += cnt
+    dp[i] += i // 2
 
 for j in range(3,10001):
     dp[j] += dp[j-3]
