6ea98537c6a831dec44f1d901573d2acf4f90458
정재원 <101786715+JaeWonna@users.noreply.github.com>
2022-07-29 17:44:16 +0900

    Update 미네랄(2933번).py

--- 백준/Python/Category/시뮬레이션/미네랄(2933번).py
+++ 백준/Python/Category/시뮬레이션/미네랄(2933번).py
@@ -1,75 +1,93 @@
-''' 수정중!! '''
-
 from collections import deque
 import sys
+
 input = sys.stdin.readline
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
 
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
+def destroy(i, left):
+    i, j = r - i, 0
+    if left == 1:
+        for k in range(c):
+            if a[i][k] == 'x':
+                a[i][k] = '.'
+                j = k
+                break
+    else:
+        for k in range(c-1, -1, -1):
+            if a[i][k] == 'x':
+                a[i][k] = '.'
+                j = k
+                break
 
-def bfs(x,y):
-    queue = deque()
-    v = [[0]*c for _ in range(r)]
-    queue.append((x,y))
-    v[x][y] = 1
+    for k in range(4):
+        ni = i + dx[k]
+        nj = j + dy[k]
+        if 0 <= ni < r and 0 <= nj < c:
+            if a[ni][nj] == 'x':
+                dq.append([ni, nj])
 
-    while queue:
-        x,y = queue.popleft()
-
+def bfs(x, y):
+    q = deque()
+    check = [[0]*c for _ in range(r)]
+    fall_list = []
+    q.append([x, y])
+    check[x][y] = 1
+    while q:
+        x, y = q.popleft()
+        if x == r-1:
+            return
+        if a[x+1][y] == '.':
+            fall_list.append([x, y])
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-        
             if 0 <= nx < r and 0 <= ny < c:
-                if a[nx][ny] != '.' and v[nx][ny] == 0:
-                    queue.append((nx,ny))
-                    v[nx][ny] = 1
+                if a[nx][ny] == 'x' and not check[nx][ny]:
+                    check[nx][ny] = 1
+                    q.append([nx, ny])
 
-def down(): # 막 하면 안된다 모양 유지시키면서 내려와야 한다
-    for i in range(r-2,-1,-1):
+    fall(check, fall_list)
+
+def fall(check, fall_list):
+    k, flag = 1, 0
+    while True:
+        for i, j in fall_list:
+            if i + k == r-1:
+                flag = 1
+                break
+            if a[i+k+1][j] == 'x' and not check[i+k+1][j]:
+                flag = 1
+                break
+        if flag:
+            break
+        k += 1
+
+    for i in range(r-2, -1, -1):
         for j in range(c):
-            if a[i][j] != '.' and a[i+1][j] == '.':
-                for k in range(i+1,r): 
-                    if k == r-1 and a[k][j] == '.':
-                        a[k][j] = a[i][j]
-                    elif a[k][j] != '.':
-                        a[k-1][j] = a[i][j]
-                        break
+            if a[i][j] == 'x' and check[i][j]:
                 a[i][j] = '.'
+                a[i+k][j] = 'x'
 
+r, c = map(int, input().split())
+a = [list(input().strip()) for _ in range(r)]
+n = int(input())
+s = list(map(int, input().split()))
+dq = deque()
 
-r,c = map(int, input().split()) # r은 높이
-a = [list(input().rstrip()) for _ in range(r)]
-n = int(input()) # 막대 던진 횟수
-h = list(map(int, input().split()))
-flag = 1
-before = flag
+left = 1
+while n:
+    index = s.pop(0)
+    destroy(index, left)
 
-for i in range(n):
-    if i % 2 == 0:
-        for j in range(c):
-            if a[r-h[i]][j] != '.':
-                a[r-h[i]][j] = '.'
-                break
-    else:
-        for j in range(c-1,-1,-1):
-            if a[r-h[i]][j] != '.':
-                a[r-h[i]][j] = '.'
-                break
+    while dq:
+        x, y = dq.popleft()
+        bfs(x, y)
 
-    for i in range(r):
-        for j in range(c):
-            if a[i][j] != '.':
-                bfs(i,j)
-    
-    # down()은 언제? 모양 유지시키면서 내려오는건 언제? 
+    left *= -1
+    n -= 1
 
-
-# 마지막 결과값
 for i in range(r):
     for j in range(c):
         print(a[i][j], end='')
     print()
-
-
-# Question) 그래프를 건들 수 없다면 모든 인덱스마다 시행해야 하는가? 무조건?
