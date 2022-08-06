7fd8c044fb64c795a35fc06c92f70884921f2eed
정재원 <101786715+JaeWonna@users.noreply.github.com>
2022-08-06 18:13:36 +0900

    Create N과 M(3) (15651번).py

---
+++ 백준/Python/Category/백트래킹/N과 M(3) (15651번).py
@@ -0,0 +1,13 @@
+def backtracking():
+    if len(s) == n:
+        print(' '.join(map(str, s)))
+        return
+    
+    for i in range(1,m+1):
+        s.append(i)
+        backtracking()
+        s.pop()
+
+m,n = map(int, input().split())
+s = []
+backtracking()
