42f2fa792bd57e2bb24644555a9aedd1e92484d9
정재원 <101786715+JaeWonna@users.noreply.github.com>
2022-08-08 12:44:49 +0900

    Create N과 M(9) (15663번).py

---
+++ 백준/Python/Category/백트래킹/N과 M(9) (15663번).py
@@ -0,0 +1,15 @@
+from itertools import permutations
+
+N, M = map(int, input().split())
+N_list = list(map(int, input().split()))
+N_list = sorted(N_list) #순서대로 나오게 정렬 먼저
+
+output = []
+for numbers in list(permutations(N_list, M)):
+    output.append(numbers)
+output = sorted(list(set(output))) #중복 제거 후 정렬 까지 한번에!
+
+for numbers in output:
+    for num in numbers:
+        print(num, end=' ')
+    print()
